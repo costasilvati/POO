dc93d0984bda29933eba32e57f3a6a845a09266a
Costa-Silva <julianacostasilvati@gmail.com>
2023-03-05 21:31:02 -0300

    Código de aula 1 e 2

---
+++ codigo/2023/aula1/Principal.java
@@ -0,0 +1,6 @@
+public class Principal{
+    public static void main(String []args){
+        String nome = "Juliana"; // Declarando uma variável
+        System.out.println("Olá! "+ nome); // Concatenando variável e texto
+    }
+}

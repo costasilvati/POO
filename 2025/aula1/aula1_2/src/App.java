1220243ea4ca583bf04e962a91d9c061e81381bb
Juliana Costa-Silva <costasilvati@users.noreply.github.com>
2025-03-11 20:55:38 -0300

    códigos de aula

---
+++ 2025/aula1/aula1_2/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String args[]){
+        int numero = 10;
+        System.out.println("Olá Java!");
+        System.out.println("Temos o número: " + numero);
+    }
+}

cf3ac5de7a5da063e23389874e71d56d887a4154
Juliana Costa-Silva <julianacostasilvati@gmail.com>
2026-04-07 18:13:25 -0300

    add: código de aula

--- 2026/06_a_08_04_2026/LojaDeLivros/src/CD.java
+++ 2026/06_a_08_04_2026/LojaDeLivros/src/CD.java
@@ -1,3 +1,27 @@
-public class CD {
+public class CD extends Produto{
+    private int faixas;
 
+    public CD(){}
+
+    public CD(String nome, double preco, int faixas){
+        super(nome, preco);
+        this.faixas = faixas;
+    }
+
+    // Getters e setters
+    public int getFaixas(){return this.faixas;}
+    public void setAutor(int duracao){this.faixas = duracao;}
+
+    // toString
+    /* 
+    public String toString(){
+        return "CD: " +super.toString() + " \t Duração: "+ this.autor;
+    }
+    */
+
+    public String toString(){
+        return "CD: [Nome: " + getNome() 
+                    + "\t Preco: "+ getPreco()
+                    +" \t Duração "+ this.faixas +"]";
+    }
 }
--- 2026/06_a_08_04_2026/LojaDeLivros/src/DVD.java
+++ 2026/06_a_08_04_2026/LojaDeLivros/src/DVD.java
@@ -1,3 +1,27 @@
-public class DVD {
+public class DVD extends Produto{
+    private int duracao;
 
+    public DVD(){}
+
+    public DVD(String nome, double preco, int duracao){
+        super(nome, preco);
+        this.duracao = duracao;
+    }
+
+    // Getters e setters
+    public int getDuracao(){return this.duracao;}
+    public void setAutor(int duracao){this.duracao = duracao;}
+
+    // toString
+    /* 
+    public String toString(){
+        return "CD: " +super.toString() + " \t Duração: "+ this.autor;
+    }
+    */
+
+    public String toString(){
+        return "DVD: [Nome: " + getNome() 
+                    + "\t Preco: "+ getPreco()
+                    +" \t Duração "+ this.duracao +"]";
+    }
 }
